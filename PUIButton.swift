5e5425dde2c2fd15d8ef1600ea3e1a091a020d0e
Kenan Atmaca <kenanatmaca@Kenan-iMac.local>
2017-06-25 01:12:39 +0300

    ...

---
+++ PUIButton.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class PUIButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("@error")
+    }
+    
+    
+
+    
+
+}//
---
+++ testVC.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class testVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+    }
+
+    
+
+}
